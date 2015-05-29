02e421b0c94e16700951c4ad81bb10603526addb
PavelCSS <yashin.ziby@gmail.com>
2015-05-30 00:30:49 +0300

    Update

--- seotoaster_core/application/app/MagicSpaces/Adminonly/Adminonly.php
+++ seotoaster_core/application/app/MagicSpaces/Adminonly/Adminonly.php
@@ -1,10 +1,11 @@
 <?php
+
 class MagicSpaces_Adminonly_Adminonly extends Tools_MagicSpaces_Abstract
 {
-	protected $_parseBefore = true;
+    protected $_parseBefore = true;
 
-	protected function _run()
-	{
-		return (Tools_Security_Acl::isAllowed(Tools_Security_Acl::RESOURCE_PLUGINS)) ? $this->_spaceContent : '';
-	}
+    protected function _run()
+    {
+        return (Tools_Security_Acl::isAllowed(Tools_Security_Acl::RESOURCE_PLUGINS)) ? $this->_spaceContent : '';
+    }
 }
